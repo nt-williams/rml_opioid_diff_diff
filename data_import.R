9b362585270e375dfb15a8491f64262ccc9bdd74
Nicholas Williams <ntwilliams.personal@gmail.com>
2018-09-24 13:47:50 -0400

    import past year opioid use datasets and put them into a list for cleaning

--- data_import.R
+++ data_import.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(readxl)
 
+# LIFETIME HEROIN USE FILES
 # importing lifetime heroin use data for 2010/11 - 2015/16
 
 lt_heroin_1011_data <- read_excel("C:/Users/niwi8/OneDrive - cumc.columbia.edu/Practicum/mj_opioid_d_in_d/data/raw/LifetimeHeroin_state_2010-16.xlsx", 
@@ -14,11 +15,6 @@ lt_heroin_1415_data <- read_excel("C:/Users/niwi8/OneDrive - cumc.columbia.edu/P
 
 lt_heroin_1516_data <- read_excel("C:/Users/niwi8/OneDrive - cumc.columbia.edu/Practicum/mj_opioid_d_in_d/data/raw/LifetimeHeroin_state_2010-16.xlsx", 
                                   sheet = "HeroinLT_state1516")
-
-# changing heroin ever use names for 1415 and 1516 data
-
-lt_heroin_1415_data <- lt_heroin_1415_data %>% rename("HEROIN EVER USED" = "EVER USED HEROIN")
-lt_heroin_1516_data <- lt_heroin_1516_data %>% rename("HEROIN EVER USED" = "EVER USED HEROIN")
 
 # creating year variable
 
@@ -35,21 +31,29 @@ lt_heroin_1516_data <- lt_heroin_1516_data %>%
 
 heroin_list <- list(lt_heroin_1011_data, lt_heroin_1213_data, lt_heroin_1415_data, lt_heroin_1516_data)
 
-# cleaning names
+# PAST YEAR OPIOID USE DISORDER FILES
+# importing past year opioid use disorder data for 2010-2016
 
-for (i in 1:length(heroin_list)) {
-  heroin_list[[i]] <- janitor::clean_names(heroin_list[[i]])
-}
+py_oud_1011_data <- read_excel("C:/Users/niwi8/OneDrive - cumc.columbia.edu/Practicum/mj_opioid_d_in_d/data/raw/PY_OUD_state_2010-16.xlsx", 
+                               sheet = "NMPO_ABDEPpy_State_1011")
 
-# cleaning data
+py_oud_1213_data <- read_excel("C:/Users/niwi8/OneDrive - cumc.columbia.edu/Practicum/mj_opioid_d_in_d/data/raw/PY_OUD_state_2010-16.xlsx", 
+                               sheet = "NMPO_ABDEPpy_State_1213")
 
-for (i in 1:length(heroin_list)) {
-  heroin_list[[i]] <- heroin_list[[i]] %>% 
-    filter(!state_fips_code_numeric %in% c("Overall", "11 - District of Columbia"),
-           heroin_ever_used == "Overall") %>%
-    separate(state_fips_code_numeric, c("fips_code", "state"),
-             sep = "-") %>%
-    select(fips_code, state, use_est = row_percent, use_se = row_percent_se, year)
-}
+py_oud_1516_data <- read_excel("C:/Users/niwi8/OneDrive - cumc.columbia.edu/Practicum/mj_opioid_d_in_d/data/raw/PY_OUD_state_2010-16.xlsx", 
+                               sheet = "NMPO_ABDEPpy_State_1516")
+
+# creating year variable for past year oud datasets
+
+py_oud_1011_data <- py_oud_1011_data %>% 
+  mutate(year = "2011")
+py_oud_1213_data <- py_oud_1213_data %>% 
+  mutate(year = "2013")
+py_oud_1516_data <- py_oud_1516_data %>% 
+  mutate(year = "2016")
+
+# putting datasets into a list
+
+py_oud_list <- list(py_oud_1011_data, py_oud_1213_data, py_oud_1516_data)
 
 
