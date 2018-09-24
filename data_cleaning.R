4129cd412398b23eab8d16368d88c3d68072905d
Nicholas Williams <ntwilliams.personal@gmail.com>
2018-09-24 14:14:08 -0400

    cleaned lifetime heroin use data

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,16 +1,30 @@
+
+library(tidyverse)
+
+# CLEANING LIFETIME HEROIN USE
+# changing heroin ever use names for 1415 and 1516 data
+
+lt_heroin_1415_data <- lt_heroin_1415_data %>% rename("HEROIN EVER USED" = "EVER USED HEROIN")
+lt_heroin_1516_data <- lt_heroin_1516_data %>% rename("HEROIN EVER USED" = "EVER USED HEROIN")
+
+# stacking data frames
+
+lt_heroin_data <- bind_rows(lt_heroin_1011_data, 
+                            lt_heroin_1213_data, 
+                            lt_heroin_1415_data, 
+                            lt_heroin_1516_data)
+
 # cleaning names
 
-for (i in 1:length(heroin_list)) {
-  heroin_list[[i]] <- janitor::clean_names(heroin_list[[i]])
-}
+lt_heroin_data <- janitor::clean_names(lt_heroin_data)
 
 # cleaning data
 
-for (i in 1:length(heroin_list)) {
-  heroin_list[[i]] <- heroin_list[[i]] %>% 
-    filter(!state_fips_code_numeric %in% c("Overall", "11 - District of Columbia"),
-           heroin_ever_used == "Overall") %>%
-    separate(state_fips_code_numeric, c("fips_code", "state"),
-             sep = "-") %>%
-    select(fips_code, state, use_est = row_percent, use_se = row_percent_se, year)
-}
+lt_heroin_data <- lt_heroin_data %>%
+  filter(
+    !state_fips_code_numeric %in% c("Overall", "11 - District of Columbia"),
+    heroin_ever_used == "Overall"
+  ) %>%
+  separate(state_fips_code_numeric, c("fips_code", "state"),
+           sep = "-") %>%
+  select(fips_code, state, use_est = row_percent, use_se = row_percent_se, year)
