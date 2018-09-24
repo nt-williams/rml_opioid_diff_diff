ba78d13484371c49b102cf6d964195f635e2a68d
Nicholas Williams <ntwilliams.personal@gmail.com>
2018-09-24 13:35:33 -0400

    imported all lifetime heroin use data

--- data_import.R
+++ data_import.R
@@ -4,7 +4,7 @@ library(readxl)
 # importing lifetime heroin use data for 2010/11 - 2015/16
 
 lt_heroin_1011_data <- read_excel("C:/Users/niwi8/OneDrive - cumc.columbia.edu/Practicum/mj_opioid_d_in_d/data/raw/LifetimeHeroin_state_2010-16.xlsx", 
-                             sheet = "HeroinLT_state1011")
+                                  sheet = "HeroinLT_state1011")
 
 lt_heroin_1213_data <- read_excel("C:/Users/niwi8/OneDrive - cumc.columbia.edu/Practicum/mj_opioid_d_in_d/data/raw/LifetimeHeroin_state_2010-16.xlsx", 
                                   sheet = "HeroinLT_state1213")
@@ -15,18 +15,41 @@ lt_heroin_1415_data <- read_excel("C:/Users/niwi8/OneDrive - cumc.columbia.edu/P
 lt_heroin_1516_data <- read_excel("C:/Users/niwi8/OneDrive - cumc.columbia.edu/Practicum/mj_opioid_d_in_d/data/raw/LifetimeHeroin_state_2010-16.xlsx", 
                                   sheet = "HeroinLT_state1516")
 
+# changing heroin ever use names for 1415 and 1516 data
+
+lt_heroin_1415_data <- lt_heroin_1415_data %>% rename("HEROIN EVER USED" = "EVER USED HEROIN")
+lt_heroin_1516_data <- lt_heroin_1516_data %>% rename("HEROIN EVER USED" = "EVER USED HEROIN")
+
+# creating year variable
+
+lt_heroin_1011_data <- lt_heroin_1011_data %>% 
+  mutate(year = "2011")
+lt_heroin_1213_data <- lt_heroin_1213_data %>% 
+  mutate(year = "2013")
+lt_heroin_1415_data <- lt_heroin_1415_data %>% 
+  mutate(year = "2015")
+lt_heroin_1516_data <- lt_heroin_1516_data %>% 
+  mutate(year = "2016")
+
+# putting datasets into a list called heroin_list
+
 heroin_list <- list(lt_heroin_1011_data, lt_heroin_1213_data, lt_heroin_1415_data, lt_heroin_1516_data)
 
+# cleaning names
 
 for (i in 1:length(heroin_list)) {
   heroin_list[[i]] <- janitor::clean_names(heroin_list[[i]])
 }
 
- 
+# cleaning data
 
-lt_heroin_2011_data <- lt_heroin_2011_data %>%
-  filter(!state_fips_code_numeric %in% c("Overall", "11 - District of Columbia"),
-         heroin_ever_used == "Overall") %>%
-  separate(state_fips_code_numeric, c("fips_code", "state"),
-           sep = "-") %>%
-  select(fips_code, state, use_est = row_percent, use_se = row_percent_se)
+for (i in 1:length(heroin_list)) {
+  heroin_list[[i]] <- heroin_list[[i]] %>% 
+    filter(!state_fips_code_numeric %in% c("Overall", "11 - District of Columbia"),
+           heroin_ever_used == "Overall") %>%
+    separate(state_fips_code_numeric, c("fips_code", "state"),
+             sep = "-") %>%
+    select(fips_code, state, use_est = row_percent, use_se = row_percent_se, year)
+}
+
+
