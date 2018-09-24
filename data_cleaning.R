d0aa4e60f913a66dd71da712071b32cf29ba6407
Nicholas Williams <ntwilliams.personal@gmail.com>
2018-09-24 14:28:50 -0400

    cleaned past year oud data

--- data_cleaning.R
+++ data_cleaning.R
@@ -4,8 +4,8 @@ library(tidyverse)
 # CLEANING LIFETIME HEROIN USE
 # changing heroin ever use names for 1415 and 1516 data
 
-lt_heroin_1415_data <- lt_heroin_1415_data %>% rename("HEROIN EVER USED" = "EVER USED HEROIN")
-lt_heroin_1516_data <- lt_heroin_1516_data %>% rename("HEROIN EVER USED" = "EVER USED HEROIN")
+lt_heroin_1415_data <- lt_heroin_1415_data %>% rename("HEROIN - EVER USED" = "EVER USED HEROIN")
+lt_heroin_1516_data <- lt_heroin_1516_data %>% rename("HEROIN - EVER USED" = "EVER USED HEROIN")
 
 # stacking data frames
 
@@ -28,3 +28,32 @@ lt_heroin_data <- lt_heroin_data %>%
   separate(state_fips_code_numeric, c("fips_code", "state"),
            sep = "-") %>%
   select(fips_code, state, use_est = row_percent, use_se = row_percent_se, year)
+
+# CLEANING PAST YEAR OPIOID USE
+# changing variable name for 2015-2016 data
+
+py_oud_1516_data <- py_oud_1516_data %>% rename(
+  "PAIN RELIEVER ABUSE OR DEPENDENCE - PAST YEAR" = "RC-PAIN RELIEVER DEPENDENCE OR ABUSE - PAST YEAR"
+  )
+
+# stacking data
+
+py_oud_data <- bind_rows(py_oud_1011_data, 
+                         py_oud_1213_data, 
+                         py_oud_1516_data)
+
+# cleaning names
+
+py_oud_data <- janitor::clean_names(py_oud_data)
+
+# cleaning data
+
+py_oud_data <- py_oud_data %>%
+  filter(
+    !state_fips_code_numeric %in% c("Overall", "11 - District of Columbia"),
+    pain_reliever_abuse_or_dependence_past_year == "Overall"
+  ) %>%
+  separate(state_fips_code_numeric, c("fips_code", "state"),
+           sep = "-") %>%
+  select(fips_code, state, use_est = row_percent, use_se = row_percent_se, year)
+
