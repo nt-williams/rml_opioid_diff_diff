1445e96316c56967cf958aa22909c5d549409e2c
Nicholas Williams <ntwilliams.personal@gmail.com>
2018-09-24 14:58:52 -0400

    clean data import

--- diff_diff_analysis.R
+++ diff_diff_analysis.R
@@ -8,3 +8,11 @@ library(tidyverse)
 
 # importing cleaned datasets
 
+lt_heroin_data <- read_csv("C:/Users/niwi8/OneDrive - cumc.columbia.edu/Practicum/mj_opioid_d_in_d/data/clean/lt_heroin_10_16.csv", 
+                           col_types = 'ccddi')
+
+py_oud_data <- read_csv("C:/Users/niwi8/OneDrive - cumc.columbia.edu/Practicum/mj_opioid_d_in_d/data/clean/py_oud_10_16.csv", 
+                        col_types = 'ccddi')
+
+nmpou_data <- read_csv("C:/Users/niwi8/OneDrive - cumc.columbia.edu/Practicum/mj_opioid_d_in_d/data/clean/nmpou_nosex_noage_10_16.csv", 
+                       col_types = 'ccddi')
